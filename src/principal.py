7e9087a232f2b6efbb770741b8065f1219630876
ElMaximato <ferbashpro@gmail.com>
2026-03-24 13:32:05 -0700

    Final: Archivo principal.py unificado y funcional

--- src/principal.py
+++ src/principal.py
@@ -1,5 +1,5 @@
-Estructura principal
-Módulo Login: Activo
+print('Estructura principal')
+print('Módulo Login: Activo')
 print('FUNCIONALIDAD: Validación de credenciales de usuario')
 print('Registro de Usuarios')
 print('FUNCIONALIDAD: Creación de nuevos perfiles en el sistema')
